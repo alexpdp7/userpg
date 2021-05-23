cecea23b7878c5777fe0e45fd9370b9da5eb7a51
alex <alex@pdp7.net>
2021-05-23 18:54:56 +0200

    Draft pgAdmin4 experiment

---
+++ config_local.py
@@ -0,0 +1,7 @@
+SQLITE_PATH = "/tmp/pgadmin4/db.sqlite"
+LOG_FILE = "/tmp/pgadmin4/log"
+SESSION_DB_PATH = "/tmp/pgadmin4/sessions"
+DATA_DIR = "/tmp/pgadmin4"
+STORAGE_DIR = "/tmp/pgadmin4"
+SERVER_MODE = False
+MASTER_PASSWORD_REQUIRED = False
